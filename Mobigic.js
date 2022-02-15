a0dc4d1f786a5a532abfdff3b3f5c751e801c5ef
Ritz1998 <ritulpatil718@gmail.com>
2022-02-15 16:55:14 +0530

    Added comments inside code

--- Mobigic.js
+++ Mobigic.js
@@ -1,18 +1,17 @@
-let body = document.querySelector("body")
-let container = document.createElement("div");
+ let body = document.querySelector("body")
+ let container = document.createElement("div");
  let originalgrid=[]
 
  let getGrids=[]
 // container.classList="container"
 
+// creating two input fields and appending it to body
 body.appendChild(container)
 container.style = 'width:90%;  height:90vh;  background:#fff ; display:flex; align-items:center ; justify-content:center; flex-direction:column; flex-wrap:wrap';
 
 let inputContainer = document.createElement("div");
 container.appendChild(inputContainer);
 container.style = ' width:80% ; min-width:500px; margin:auto ;background:#09e; height:200px; display:flex; flex-direction:column; align-items:center ; justify-content:center;  flex-wrap:wrap ;border-radius:10px; box-shadow:0 6px 13px #00000099  ';
-
-
 
 let rowInput = document.createElement("input");
 rowInput.setAttribute("type", "number")
@@ -24,7 +23,7 @@ columnInput.style = css
 columnInput.setAttribute("type", "number")
 columnInput.setAttribute("placeholder", "Please Enter Number Of Desired Columns")
 
-
+// creating div to create grid inside
 let gridholder = document.createElement("div");
 gridholder.style = ' display:grid; align-items:center; margin:20px ';
 body.appendChild(gridholder);
@@ -33,6 +32,7 @@ let searchholder =document.createElement("div")
 
 searchholder.style='display:inline-block; margin:20px 0;'
 
+//creating search field to search letter inside grids
 let search=document.createElement("input");
 search.setAttribute("type","text");
 search.setAttribute("placeholder","Enter to search word");
@@ -45,6 +45,8 @@ searchbutton.value=search
 searchbutton.style='padding:5px; margin:0 10px; border-radius:5px'
 searchholder.appendChild(searchbutton)
 
+
+//creating button to show desired 2D grids On Click
 let button = document.createElement("button");
 button.innerHTML = "Create"
 button.style = 'width:100px; background:#999 ;padding:8px 3px ; font-weight:bold; border-radius:10px ; float:right; margin:20px 0'
@@ -67,23 +69,19 @@ button.addEventListener("click", () => {
     let c = rowInput.value;
     let r = columnInput.value
 
-   
     // console.log(grids);
     for (var i = 0; i < c; i++) {
         originalgrid[i]=[ ];
         getGrids[i]=[ ];
         for (var j = 0; j < r; j++) {
             let g = document.createElement("div")
-           let char= alphas.charAt(Math.floor(Math.random() *
-            alphas.length));
+           let char= alphas.charAt(Math.floor(Math.random() * alphas.length));
             g.style = 'border: 1px solid black;  display:inline-block ; width:50px;height:50px; text-align:center;line-height:50px;'
             g.innerHTML =char
             grids.appendChild(g)
             
             originalgrid[i][j]=char
-            getGrids[i][j]=g
-
-                ;
+            getGrids[i][j]=g;
         }
         grids.appendChild(document.createElement("br"))
     }
@@ -99,9 +97,13 @@ inputContainer.appendChild(button)
 inputContainer.appendChild(searchholder)
 
 
+//creating a function to search a word in grids
+
 function searchword(findword) {
    
     let R, C;
+
+    //here i am defining thedirections to search word in grids
 
     let x = [-1, -1, -1, 0, 0, 1, 1, 1];
 
@@ -114,35 +116,24 @@ function searchword(findword) {
 
         let len = word.length;
 
-        // Search word in all 8 directions
-        // starting from (row, col)
         for (let dir = 0; dir < 8; dir++) {
             // Initialize starting point
             // for current direction
             let k, rd = row + x[dir], cd = col + y[dir];
             
             // getGrids[row][col].style.color='green'
-            // First character is already checked,
-            // match remaining characters
+            
             for (k = 1; k < len; k++) {
-                // If out of bound break
+               
                 if (rd >= R || rd < 0 || cd >= C || cd < 0)
                     break;
 
-                // If not matched, break
                 if (grid[rd][cd] != word[k])
                     break;
-
-                // Moving in particular direction
-                // getGrids[rd][cd].style.color='green'
                 rd += x[dir];
                 cd += y[dir];
             }
            
-
-            // If all character matched,
-            // then value of must
-            // be equal to length of word
             if (k == len){
             // getGrids[row][col].style.color='red'
                 return true;
@@ -151,11 +142,8 @@ function searchword(findword) {
         return false;
     }
 
-    // Searches given word in a given
-    // matrix in all 8 directions
     function patternSearch(grid, word) {
-        // Consider every point as starting
-        // point and search given word
+      
         for (let row = 0; row < R; row++) {
             for (let col = 0; col < C; col++) {
                 for(let i=0;i<getGrids.length;i++){
@@ -173,14 +161,15 @@ function searchword(findword) {
             }
         }
     }
-  // Driver code
+
+
     R = rowInput.value;
     C = columnInput.value;
     let grid =originalgrid;
-    // console.log(R,C,grid);
+
     patternSearch(grid, findword);
   
-    // patternSearch(grid, "EC");
+  
 
 }
 
